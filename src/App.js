0f356e3ea88803cd08f3222d8ef346519b26c618
SaySayo <sayobamigbade@gmail.com>
2021-12-04 18:55:46 +0100

    Revert "Merge branch 'main' of https://github.com/SaySayo/BioTechnics into main"
    This reverts commit ff8ab9ce586c3d42d2bdaee8ec37f518764b710a, reversing
    changes made to a8bf0fb3fd370832177854e357a60e5b7653c47e.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Route, Switch } from 'react-router';
 import './App.css';
 import { Home } from './pages/Home';
 import { Signup } from './pages/Signup';
-import Explore from './pages/Explore/Explore';
-import HumanEye from "./pages/HumanEye/HumanEye";
-import HumanBrain from "./pages/HumanBrain/HumanBrain";
-import HumanHeart from "./pages/HumanHeart/HumanHeart";
 
 function App() {
   return (
-    <Router>
     <Switch>
-      <Route path={'/'} exact component={Home} />
-      <Route exact path={'/signup'} component={Signup} />
-      <Route exact path='/explore' component={Explore} />
-      <Route exact path='/humaneye' component={HumanEye} />
-      <Route exact path='/brain' component={HumanBrain} />
-      <Route exact path='/heart' component={HumanHeart} />
+        <Route exact path={'/'} component={Home} />
+        <Route exact path={'/signup'} component={Signup} />
     </Switch>
-  </Router>
     
   );
 }
